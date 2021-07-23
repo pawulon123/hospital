89223bda4f091d3279575761b716c1fabc4c7961
pawelchodzba123 <pawul0n@op.pl>
2021-07-23 14:22:19 +0200

    create a ConfigModule, .env, DB connect

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import configDb from './config.db';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot(configDb()),
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
