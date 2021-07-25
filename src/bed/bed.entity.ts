3eb82770771fa8c30147248f551daa9480ad16b5
pawelchodzba123 <pawul0n@op.pl>
2021-07-25 14:05:11 +0200

    create bed entity

--- src/bed/bed.entity.ts
+++ src/bed/bed.entity.ts
@@ -1,1 +1,17 @@
-export class BedEntity {}
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity({ name: "bed" })
+export class BedEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+    @Column()
+    id_room: number;
+    @Column({ type: "int", nullable: true })
+    x_svg: number;
+    @Column({ type: "int", nullable: true })
+    y_svg: number;
+    @Column({ type: "int", nullable: true })
+    rotate: number;
+    @Column({ type: "varchar", length: 20, default:"basic" })
+    type: string;
+}
