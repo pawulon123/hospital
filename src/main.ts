66e483d5d7da9fcceb69efa4176564d4d578ccb6
pawelchodzba123 <pawul0n@op.pl>
2021-08-30 11:32:53 +0200

    apply CORS

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,14 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import  {helmetCustomer as helmet}  from './cofig/HttpHeadersHelmet';
+import * as helmet from 'helmet';
 
-async function bootstrap() { console.log(helmet);
-
+async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   (app as NestExpressApplication).use(helmet());
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe({
-    disableErrorMessages: false,
+    disableErrorMessages: true,
     whitelist: true,
     forbidNonWhitelisted: true
   }));
